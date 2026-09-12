1371bf7d4313c3b19d89130bc785d6e881023ddd
shadowcrafter <sharkpuncher08@gmail.com>
2026-09-12 15:43:04 -0400

    Start bot loop

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from numpy.typing import NDArray
 from pathlib import Path
 import pyautogui
 from pynput import mouse
+import random
 from src.area import AreaSelector
 from src.word_finder import WordFinder
+import time
+
 
 def get_arrow_location() -> list:
     input("When ready, press enter, then click the yellow arrow WHILE IT IS POINTING at your player.")
@@ -22,10 +25,11 @@ def get_arrow_location() -> list:
 
     return pos
 
-def get_image() -> MatLike | None:
+
+def get_letter_region() -> tuple | None:
     Path("logs/").mkdir(parents=True, exist_ok=True)
 
-    input("Press enter when you are ready to select area for words.")
+    input("Press enter when you are ready to select area for letters.")
 
     selector = AreaSelector()
     region = selector.get_selection()
@@ -33,19 +37,46 @@ def get_image() -> MatLike | None:
         print("Could not get region from selection")
         return None
 
-    screenshot = pyautogui.screenshot("logs/region_screenshot.png", region=region)
-    img = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+    return region
 
-    return img
+
+def get_letters_in_region(img: MatLike) -> str:
+    return "hy"
+
+
+def type_word(word: str):
+    for c in word:
+        pyautogui.press(c)
+        time.sleep(random.random() / 100)
+
+
+def bot_loop():
+    mode = input("Enter mode (normal, shortest, longest): ").lower()
+    if mode == "":
+        mode = "normal"
+    elif mode != "normal" and mode != "shortest" and mode != "longest":
+        print("Invalid mode!")
+        return
+    
+    word_finder = WordFinder(mode)
+    arrow_location = get_arrow_location()
+    arrow_color = pyautogui.pixel(arrow_location[0], arrow_location[1])
+    letter_region = get_letter_region()
+
+    while True:
+        if pyautogui.pixel(arrow_location[0], arrow_location[1]) == arrow_color:
+            screenshot = pyautogui.screenshot("logs/region_screenshot.png", region=letter_region)
+            img = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+            letters = get_letters_in_region(img)
+
+            word = word_finder.get_word(letters)
+            print(f"Word found: {word}")
+            type_word(word)
+            time.sleep(0.35) #delay before checking again
 
 
 def main():
-    word_finder = WordFinder("shortest")
-    for _ in range(50):
-        print(f"word: {word_finder.get_word("hyper")}")
-
-    get_image()
-    print(get_arrow_location())
+    bot_loop()
 
 
 if __name__ == "__main__":
