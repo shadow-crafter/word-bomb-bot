e50715e5c9d0f6d8467cd5914be7890e180d3f8a
shadowcrafter <sharkpuncher08@gmail.com>
2026-09-12 15:26:59 -0400

    Add arrow detector

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ import numpy as np
 from numpy.typing import NDArray
 from pathlib import Path
 import pyautogui
+from pynput import mouse
 from src.area import AreaSelector
 from src.word_finder import WordFinder
+
+def get_arrow_location() -> list:
+    input("When ready, press enter, then click the yellow arrow WHILE IT IS POINTING at your player.")
+
+    pos: list = []
+    def on_click(x, y, _, pressed):
+        if pressed:
+            pos.extend([x, y])
+            return False
+
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join() #join so it blocks until click
+
+    return pos
 
 def get_image() -> MatLike | None:
     Path("logs/").mkdir(parents=True, exist_ok=True)
@@ -23,12 +38,14 @@ def get_image() -> MatLike | None:
 
     return img
 
+
 def main():
     word_finder = WordFinder("shortest")
     for _ in range(50):
         print(f"word: {word_finder.get_word("hyper")}")
 
     get_image()
+    print(get_arrow_location())
 
 
 if __name__ == "__main__":
